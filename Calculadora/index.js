718994a13fbb86af9423795a49204f3b71c5d8eb
Matheus Antunes <mat.ams2006@gmail.com>
2024-10-07 14:22:21 -0300

    divisao e multiplicaçao

--- Calculadora/index.js
+++ Calculadora/index.js
@@ -4,6 +4,8 @@ let reset = document.getElementById('reset')
 let igual = document.getElementById('igual')
 let mais = document.getElementById('mais')
 let menos = document.getElementById('menos')
+let divisao = document.getElementById('divisao')
+let multiplicacao = document.getElementById('multiplicacao')
 let primeiroBotao = null;
 let valor = null;
 let clique = 1;
@@ -18,19 +20,23 @@ botoes.forEach(function (botao) {
 
         texto.innerHTML += botao.textContent;
 
-
         console.log('primeiraParte', primeiraParte)
 
-        if(estouNaSugundaParte == 1) {
-          segundaParte = segundaParte + botao.textContent
-          console.log('segundaParte', segundaParte)
-        } else if (botao === mais || botao === menos) {
+        if (estouNaSugundaParte == 1) {
+            segundaParte = segundaParte + botao.textContent
+            console.log('segundaParte', segundaParte)
+        } else if (botao === mais || botao === menos || botao === divisao || botao === multiplicacao) {
             if (botao.textContent == '+') {
                 operador = mais
                 estouNaSugundaParte = 1
-
-            } else {
+            } else if (botao.textContent == '-') {
                 operador = menos
+                estouNaSugundaParte = 1
+            } else if (botao.textContent == 'x') {
+                operador = multiplicacao
+                estouNaSugundaParte = 1
+            } else if (botao.textContent == ':') {
+                operador = divisao
                 estouNaSugundaParte = 1
             }
         } else {
@@ -49,13 +55,18 @@ reset.addEventListener('click', function () {
 });
 
 igual.addEventListener('click', function () {
-    if(operador == mais) {
+    if (operador == mais) {
         let soma = Number(primeiraParte) + Number(segundaParte)
         texto.innerHTML = soma
-        
-    } else {
+    } else if (operador == menos) {
         let diminui = Number(primeiraParte) - Number(segundaParte)
         texto.innerHTML = diminui
+    } else if (operador == multiplicacao) {
+        let multiplica = Number(primeiraParte) * Number(segundaParte)
+        texto.innerHTML = multiplica
+    } else if (operador == divisao) {
+        let divide = Number(primeiraParte) / Number(segundaParte)
+        texto.innerHTML = divide
     }
     estouNaSugundaParte = 0
 });
