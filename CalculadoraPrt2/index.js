b1f6dbf86c750f5ef9cc0f45c3aee0a40a8b2a01
Wiloyy <mat.ams2006@gmail.com>
2024-10-10 17:57:11 -0300

    Criaçao de objeto, novo modo de organizaçao

--- CalculadoraPrt2/index.js
+++ CalculadoraPrt2/index.js
@@ -1,98 +1,99 @@
-let buttons = document.querySelectorAll('.buttons')
-let text = document.getElementById('text')
-let reset = document.getElementById('reset')
-let equals = document.getElementById('equals')
-let plus = document.getElementById('plus')
-let minus = document.getElementById('minus')
-let division = document.getElementById('division')
-let multiplication = document.getElementById('multiplication')
+let buttons = document.querySelectorAll('.buttons');
+let text = document.getElementById('text');
+let reset = document.getElementById('reset');
+let equals = document.getElementById('equals');
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
+let division = document.getElementById('division');
+let multiplication = document.getElementById('multiplication');
+
 let calculadora = {
-    firstButton: null,
-    value: null,
-    click: 1,
-    operator: '',
+    firstPart: '',
     secondPart: '',
-    isAtSecondPart: null,
-    firstClickExecuted: false,
-    operatorCliked: false
+    operator: '',
+    isAtSecondPart: false,
+    firstClickExecuted: false
+};
+
+function clickNumBotao(calculadora, button) {
+    if (text.innerHTML === '') {
+        if (button === division || button === multiplication) {
+            return;
+        }
+    }
+
+    if (calculadora.isAtSecondPart == 1) {
+        if(button === plus || button === minus || button === division || button === multiplication){
+            return;
+        }
+        calculadora.secondPart = calculadora.secondPart + button.textContent
+    } else if (calculadora.firstClickExecuted == true) {
+        if (button === plus || button === minus || button === division || button === multiplication) {
+            if(calculadora.firstPart == '+' || calculadora.firstPart == '-' ||  calculadora.firstPart == 'x' || calculadora.firstPart == ':' ){
+                return;
+            }
+            calculadora.operator = button;
+            calculadora.isAtSecondPart = 1;
+            calculadora.firstClickExecuted = false
+        } else {
+            calculadora.firstPart = calculadora.firstPart + button.textContent
+        }
+    } else {
+        calculadora.firstPart = calculadora.firstPart + button.textContent
+        calculadora.firstClickExecuted = true
+    }
+    text.innerHTML += button.textContent;
+    console.log(`######`)
+    console.log('primeira', calculadora.firstPart)
+    console.log('operador',calculadora.operator)
+    console.log('segunda', calculadora.secondPart)
+    console.log('isAtSecondPart', calculadora.isAtSecondPart)
+    console.log('firstClickExecuted', calculadora.firstClickExecuted)
+
 }
 
+function clickNoIgual(calculadora) {
+    let result;
+    
+    if (calculadora.operator === plus) {
+        result = Number(calculadora.firstPart) + Number(calculadora.secondPart);
+    } else if (calculadora.operator === minus) {
+        result = Number(calculadora.firstPart) - Number(calculadora.secondPart);
+    } else if (calculadora.operator === multiplication) {
+        result = Number(calculadora.firstPart) * Number(calculadora.secondPart);
+    } else if (calculadora.operator === division) {
+        result = Number(calculadora.firstPart) / Number(calculadora.secondPart);
+    } else {
+        return; 
+    }
+
+    text.innerHTML = result; 
+    calculadora.firstPart = result
+    calculadora.secondPart = ''; 
+    calculadora.operator = ''; 
+    calculadora.isAtSecondPart = false; 
+    calculadora.firstClickExecuted = true; 
+}
+
+function clickNoClear(calculadora) {
+    text.innerHTML = '';
+    calculadora.firstPart = '';
+    calculadora.secondPart = '';
+    calculadora.operator = '';
+    calculadora.isAtSecondPart = false;
+    calculadora.firstClickExecuted = false;
+}
 
 buttons.forEach(function (button) {
     button.addEventListener('click', function () {
-        if (text.innerHTML === '') {
-            if (button === division || button === multiplication) {
-                return;
-            }
-        }
-
-        if (isAtSecondPart == 1) {
-            if (button === plus || button === minus || button === division || button === multiplication) {
-                return;
-            }
-            secondPart = secondPart + button.textContent
-        } else if (firstClickExecuted == true) {
-            if (button === plus || button === minus || button === division || button === multiplication) {
-                if (firstPart == '+' || firstPart == '-' || firstPart == 'x' || firstPart == ':') {
-                    return;
-                }
-                operator = button;
-                isAtSecondPart = 1;
-                firstClickExecuted = false
-            } else {
-                firstPart = firstPart + button.textContent
-            }
-        } else {
-            firstPart = firstPart + button.textContent
-            firstClickExecuted = true
-        }
-        text.innerHTML += button.textContent;
-        console.log(`######`)
-        console.log('primeira', firstPart)
-        console.log('operador', operator)
-        console.log('segunda', secondPart)
-        console.log('isAtSecondPart', isAtSecondPart)
-        console.log('firstClickExecuted', firstClickExecuted)
-
+        clickNumBotao(calculadora, button);
     });
 });
 
 reset.addEventListener('click', function () {
-    text.innerHTML = ''
-    firstPart = ''
-    secondPart = ''
-    operator = ''
-    isAtSecondPart = 0
-    firstClickExecuted = false
+    clickNoClear(calculadora);
 });
 
 equals.addEventListener('click', function () {
-    let result;
-    if (operator == plus) {
-        result = Number(firstPart) + Number(secondPart)
-        text.innerHTML = result;
-    } else if (operator == minus) {
-        result = Number(firstPart) - Number(secondPart)
-        text.innerHTML = result;
-    } else if (operator == multiplication) {
-        result = Number(firstPart) * Number(secondPart)
-        text.innerHTML = result;
-    } else if (operator == division) {
-        result = Number(firstPart) / Number(secondPart)
-        text.innerHTML = result;
-    }
-
-    firstPart = result;
-    secondPart = '';
-    operator = '';
-    isAtSecondPart = 0;
-    firstClickExecuted = true
-    /*
-    console.log(`######`)
-    console.log('primeira', firstPart)
-    console.log('operador', operator)
-    console.log('segunda', secondPart)
-    console.log('isAtSecondPart', isAtSecondPart)
-    console.log('firstClickExecuted', firstClickExecuted)
-    */
+    clickNoIgual(calculadora);
 });
