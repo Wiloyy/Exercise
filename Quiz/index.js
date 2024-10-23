9199278a902b41ce99b121217ab94a109e4fd336
Wiloyy <mat.ams2006@gmail.com>
2024-10-23 17:53:12 -0300

    GamePaused has been removed. Now you can make different choices and have only one back option, and your choice turns green

--- Quiz/index.js
+++ Quiz/index.js
@@ -15,14 +15,15 @@ optionC.letter = 'C';
 let optionD = document.getElementById('optionD');
 optionD.letter = 'D';
 
-let options = [optionA, optionB, optionC, optionD]
+let options = [optionA, optionB, optionC, optionD];
 let start = document.getElementById('start');
-let backToTheSame = document.getElementById('backToTheSame');
 
 let gamestate = {
     overwatchQuestionCurrentIndex: 0,
     score: 0,
-    gamePaused: false
+    buttonClicked: false,
+    lastButtonClicked: null,
+    penultimateButton: null
 };
 
 nextButton.style.display = 'none';
@@ -30,37 +31,29 @@ resetButton.style.display = 'none';
 backButton.style.display = 'none';
 
 options.forEach(function (option) {
+    option.style.color = 'black';
     option.addEventListener('click', function () {
-        if (gamestate.gamePaused) {
-            return;
-        }
-        overwatchQuestions[gamestate.overwatchQuestionCurrentIndex].choice = option.letter;
-        text.innerHTML = `Sua resposta foi '${option.textContent}'. Você pode seguir para a próxima pergunta ou voltar e alterar sua resposta.`;
+        gamestate.penultimateButton = gamestate.lastButtonClicked;
+        gamestate.lastButtonClicked = option;
 
-        gamestate.gamePaused = true;
-        nextButton.style.display = 'block';
-        backButton.style.display = 'block';
-        backToTheSame.style.display = 'block';
+        if (gamestate.penultimateButton !== null) {
+            gamestate.penultimateButton.style.color = 'black';
+        }
+
+        console.log(gamestate.penultimateButton)
+        console.log(gamestate.lastButtonClicked)
+        
+        option.style.color = 'green';
+        overwatchQuestions[gamestate.overwatchQuestionCurrentIndex].choice = option.letter;
+        cleanOption();
     });
 });
 
 start.addEventListener('click', function () {
     if (gamestate.overwatchQuestionCurrentIndex < overwatchQuestions.length) {
         mostraTexto();
-        gamestate.gamePaused = false;
     }
-    start.style.display = 'none';
-    nextButton.style.display = 'none';
-    resetButton.style.display = 'none';
-    backButton.style.display = 'none';
-    backToTheSame.style.display = 'none';
-    lilText.innerHTML = '';
-    questionResponses.forEach(function (response) {
-        response.style.display = 'block';
-        response.style.display = 'grid';
-        response.style.gridTemplateColumns = 'repeat(2, 1fr)';
-        response.style.gap = '10px';
-    });
+    cleanStart();
 });
 
 nextButton.addEventListener('click', function () {
@@ -69,20 +62,15 @@ nextButton.addEventListener('click', function () {
     gamestate.overwatchQuestionCurrentIndex++;
 
     if (gamestate.overwatchQuestionCurrentIndex < overwatchQuestions.length) {
+        resetOptionsColor();
         mostraTexto();
-        gamestate.gamePaused = false;
-        nextButton.style.display = 'none';
-        backButton.style.display = 'none';
-        backToTheSame.style.display = 'none';
-        console.log(overwatchQuestions[gamestate.overwatchQuestionCurrentIndex])
-        console.log(gamestate.overwatchQuestionCurrentIndex)
+        cleanNext1();
     } else {
         overwatchQuestions.forEach(function (question) {
-            console.log(question)
             if (question.answer == question.choice) {
-                gamestate.score++
+                gamestate.score++;
             }
-        })
+        });
 
         if (gamestate.score === 7) {
             text.innerHTML = `Você acertou ${gamestate.score}, você sabe tudo, parabéns!`;
@@ -94,40 +82,25 @@ nextButton.addEventListener('click', function () {
             text.innerHTML = `Você acertou ${gamestate.score}, você sabe bastante, porém nem tudo. Mas mesmo assim, parabéns!`;
         }
 
-        // Atualização da interface
-        nextButton.style.display = 'none';
-        backButton.style.display = 'none';
-        resetButton.style.display = 'block';
-        backToTheSame.style.display = 'none';
-        questionResponses.forEach(function (response) {
-            response.style.display = 'none';
-        });
+        cleanNext2();
         lilText.innerHTML = 'Se quiser jogar novamente clique em reset : )';
     }
 });
-
 
 backButton.addEventListener('click', function () {
     if (gamestate.overwatchQuestionCurrentIndex > 0) {
         gamestate.overwatchQuestionCurrentIndex--;
     }
+    resetOptionsColor();
     mostraTexto();
-    gamestate.gamePaused = false;
-    backButton.style.display = 'none';
-    nextButton.style.display = 'none';
-    backToTheSame.style.display = 'none';
+    cleanBack();
 });
 
 resetButton.addEventListener('click', function () {
     gamestate.overwatchQuestionCurrentIndex = 0;
     gamestate.score = 0;
-    gamestate.gamePaused = false;
 
-    start.style.display = 'block';
-    nextButton.style.display = 'none';
-    resetButton.style.display = 'none';
-    backButton.style.display = 'none';
-    backToTheSame.style.display = 'none';
+    cleanReset();
 
     nextButton.innerHTML = 'Próximo';
     lilText.innerHTML = 'Clique em iniciar para começar o quiz. Espero que você goste! :)';
@@ -136,14 +109,8 @@ resetButton.addEventListener('click', function () {
     questionResponses.forEach(function (response) {
         response.style.display = 'none';
     });
-});
 
-backToTheSame.addEventListener('click', function () {
-    mostraTexto();
-    gamestate.gamePaused = false;
-    backButton.style.display = 'none';
-    nextButton.style.display = 'none';
-    backToTheSame.style.display = 'none';
+    resetOptionsColor();
 });
 
 function mostraTexto() {
@@ -153,4 +120,58 @@ function mostraTexto() {
     optionB.textContent = `B: ${option.B}`;
     optionC.textContent = `C: ${option.C}`;
     optionD.textContent = `D: ${option.D}`;
+}
+
+function resetOptionsColor() {
+    options.forEach(function (option) {
+        option.style.color = 'black';
+    });
+}
+
+function cleanStart() {
+    start.style.display = 'none';
+    nextButton.style.display = 'none';
+    resetButton.style.display = 'none';
+    backButton.style.display = 'none';
+    lilText.innerHTML = '';
+    questionResponses.forEach(function (response) {
+        response.style.display = 'block';
+        response.style.display = 'grid';
+        response.style.gridTemplateColumns = 'repeat(2, 1fr)';
+        response.style.gap = '10px';
+    });
+}
+
+function cleanNext1() {
+    nextButton.style.display = 'none';
+    backButton.style.display = 'none';
+}
+
+function cleanNext2() {
+    nextButton.style.display = 'none';
+    backButton.style.display = 'none';
+    resetButton.style.display = 'block';
+    questionResponses.forEach(function (response) {
+        response.style.display = 'none';
+    });
+}
+
+function cleanBack() {
+    backButton.style.display = 'none';
+    nextButton.style.display = 'none';
+}
+
+function cleanReset() {
+    start.style.display = 'block';
+    nextButton.style.display = 'none';
+    resetButton.style.display = 'none';
+    backButton.style.display = 'none';
+    questionResponses.forEach(function (response) {
+        response.style.display = 'none';
+    });
+}
+
+function cleanOption() {
+    nextButton.style.display = 'block';
+    backButton.style.display = 'block';
 }
